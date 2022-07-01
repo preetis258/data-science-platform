f2e1c53019de97f3ed15bc0660fe8fcb5bb4725f
Preetis258 <preeti.sh258@gmail.com>
2022-07-01 14:46:54 +0530

    changed var name in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-import DataAnalysis.app1
-import ModelBuilding.app2
+from DataAnalysis import app1
+from ModelBuilding import app2
 
 st.sidebar.markdown("# Browse a file:")
 uploaded_file = st.sidebar.file_uploader(" ")
@@ -10,15 +10,15 @@ container1.header( "Data Science Platform\n" )
 st.image("DS-image.jpg")
 container2= st.empty()
 container2.subheader( "Upload a dataset, select a task in the left navigation bar and get things done!" )
-codecs=['latin_1','utf_8','ISO-8859-1']
+encodings=['latin_1','utf_8','ISO-8859-1']
 global df
 if uploaded_file is not None:
     file_type = uploaded_file.name.split( '.' )[-1]
 
     if file_type=='csv':
-        for x in range( len( codecs ) ):
+        for x in range( len( encodings ) ):
             try:
-                df = pd.read_csv(uploaded_file,encoding = (codecs[x]),sep='[:,|;]',error_bad_lines=False)
+                df = pd.read_csv(uploaded_file,encoding = (encodings[x]),sep='[:,|;]',error_bad_lines=False)
             except:
                 pass
     elif file_type=='xlsx':
@@ -32,6 +32,6 @@ if uploaded_file is not None:
     container1.empty()
     container2.empty()
     if choice=='Data Analysis':
-        DataAnalysis.app1.app_data_analysis(df)
+        app1.app_data_analysis(df)
     elif choice=='Model Building':
-        ModelBuilding.app2.app_model_building(df)
+        app2.app_model_building(df)
